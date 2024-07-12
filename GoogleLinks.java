fb73a507f17f1695c719d19de479a6dd100583a2
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-07-12 22:52:34 +0530

    Fixed the defect #324 for ArrayList syntax

---
+++ GoogleLinks.java
@@ -0,0 +1,49 @@
+package scripts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class GoogleLinks {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+  @Test
+  public void loginToNicheTourism() throws InterruptedException {
+	driver.get("https://www.google.com");   
+	List<WebElement> allGoogleLinks = driver.findElements(By.xpath("//a"));
+	System.out.println("Total Links on Google.com are -" + allGoogleLinks.size());
+	 
+	for (WebElement oneElement: allGoogleLinks) {
+		System.out.println(oneElement.getText() + " - " +oneElement.getAttribute("href"));
+		 //Thread.sleep(3000);
+	}
+  }
+  
+
+  @AfterClass
+  public void afterClass() {
+	  //close the browser.
+	  driver.quit();
+  }
+
+
+}
